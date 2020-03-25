0230391036d42200653d830e9ce8a0bdcd13fd9d
Karen Alexandra Urrea Coronado <55495447+KarenAlexa99@users.noreply.github.com>
2020-03-25 18:44:29 -0400

    kjjlk

---
+++ Arcanissimum/Assets/Scripts/CameraChangeinverse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChangeinverse : MonoBehaviour
+{
+    public GameObject ThirdCam;
+    public GameObject FirstCam;
+    public int CamMode;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.ToString() == "Muro")
+        {
+            if (CamMode == 0)
+            {
+                CamMode = 1;
+            }
+            else
+            {
+                CamMode -= 1;
+            }
+        }
+    }
+}
+
---
+++ Arcanissimum/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/* This component moves our player using a NavMeshAgent. */
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{
+
+	
+	NavMeshAgent agent;     // Reference to our agent
+
+	// Get references
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	
+
+	public void MoveToPoint(Vector3 point)
+	{
+		agent.SetDestination(point);
+	}
+
+	
+
+}
